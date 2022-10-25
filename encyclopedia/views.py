73e9d21ceb75998806e989df20ab9fbca1b04042
“Lee <leesheppard2404@gmail.com>
2022-10-25 15:04:03 +0200

    added entry and no result page with functionality

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
+markdowner = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    content = util.get_entry(entry)
+
+    if content is not None:
+        return render(request, "encyclopedia/content.html", {
+            "content": markdowner.convert(content)
+        })
+    else:
+        return render(request, "encyclopedia/noresult.html", {
+            "entry": entry
+        })
